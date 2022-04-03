d0bb03cbbca933f84f4e96179450d5007fef9ded
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-03 20:50:52 -0300

    Add files via upload

---
+++ assets/preload/data/winter-melon/script.lua
@@ -0,0 +1,20 @@
+function onCreate()
+makeLuaSprite('introtext', 'texts/text-melon', -650, 0);
+setProperty('introtext.scale.x', 0.50)
+setProperty('introtext.scale.y', 0.50)
+setObjectCamera('introtext', 'other');
+addLuaSprite('introtext', true);
+end
+
+function onStepHit()
+    if curStep >= 0 then
+        doTweenX('textTween', 'introtext', -150, 1.1, 'quintOut')
+    end
+    
+    if curStep >= 64 then
+        doTweenAngle('textspin', 'introtext', 720, 1.1, 'quintOut')
+        doTweenAlpha('textfade', 'introtext', 0, 1.1, 'quintOut')
+        doTweenX('textscalex', 'introtext.scale', 0, 1.1, 'quintOut')
+        doTweenY('textscaley', 'introtext.scale', 0, 1.1, 'quintOut')
+    end
+end
